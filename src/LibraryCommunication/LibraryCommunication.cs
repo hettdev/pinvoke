0b343abf7feede8da5211379fbd1222651f3be40
Adrian Hett <adrian.hett@turtlebeach.com>
2021-07-28 11:20:11 +0200

    add empty project

---
+++ src/LibraryCommunication/LibraryCommunication.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryCommunication
+{
+    public class LibraryCommunication
+    {
+    }
+}
---
+++ src/LibraryCommunication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryCommunication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
